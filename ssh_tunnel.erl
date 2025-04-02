b3226ffd85b63cb7826947f9a640d670d235e2a3
Torbjorn Tornkvist <ttornkvi@cisco.com>
2025-04-02 16:58:45 +0000

    Modifying the tunnel code

--- ssh_tunnel.erl
+++ ssh_tunnel.erl
@@ -1,41 +1,62 @@
 -module(ssh_tunnel).
--export([local/5, remote/4, stop/1]).
+
+-export([local/6, remote/6, stop/1]).
 
 -record(tunnel, {port, type}).
 
 %% Start local port forwarding tunnel (e.g. local:8080 -> remote:80)
-local(LocalPort, RemoteHost, RemotePort, SshHost, Options) when is_integer(LocalPort),
-                                                               is_list(RemoteHost),
-                                                               is_integer(RemotePort),
-                                                               is_list(SshHost),
-                                                               is_list(Options) ->
+local(LocalIp, LocalPort, RemoteIp, RemotePort, SshHost, Options)
+  when is_list(LocalIp),
+       is_integer(LocalPort),
+       is_list(RemoteIp),
+       is_integer(RemotePort),
+       is_list(SshHost),
+       is_list(Options) ->
+    assert_ip_address(local_ip, LocalIp),
+    assert_ip_address(remote_ip, RemoteIp),
     User = proplists:get_value(user, Options),
-    Identity = case proplists:get_value(identity, Options) of
-        undefined -> "";
-        Path -> [" -i ", Path]
-    end,
+    Identity =
+        case proplists:get_value(identity, Options) of
+            undefined ->
+                "";
+            Path ->
+                [" -i ", Path]
+        end,
     % Build SSH command for local forwarding
-    Cmd = lists:flatten(io_lib:format(
-        "ssh ~s-nNT -L ~b:~s:~b ~s@~s",
-        [Identity, LocalPort, RemoteHost, RemotePort, User, SshHost]
-    )),
+    Cmd = lists:flatten(
+              io_lib:format("ssh ~s-nNT -L ~s:~b:~s:~b ~s@~s",
+                            [Identity, LocalIp, LocalPort, RemoteIp, RemotePort, User, SshHost])),
     start_tunnel(Cmd, local).
 
+assert_ip_address(What, IpStr) when is_list(IpStr) ->
+    case inet:parse_address(IpStr) of
+        {ok, _Ip} -> ok;
+        _ -> throw({error, {What,IpStr}})
+    end.
+
+
 %% Start remote port forwarding tunnel (e.g. remote:8080 -> local:80)
-remote(RemotePort, LocalPort, SshHost, Options) when is_integer(RemotePort),
-                                                    is_integer(LocalPort),
-                                                    is_list(SshHost),
-                                                    is_list(Options) ->
+remote(LocalIp, LocalPort, RemoteIp, RemotePort, SshHost, Options)
+    when is_list(LocalIp),
+         is_integer(LocalPort),
+         is_list(RemoteIp),
+         is_integer(RemotePort),
+         is_list(SshHost),
+         is_list(Options) ->
+    assert_ip_address(local_ip, LocalIp),
+    assert_ip_address(remote_ip, RemoteIp),
     User = proplists:get_value(user, Options),
-    Identity = case proplists:get_value(identity, Options) of
-        undefined -> "";
-        Path -> [" -i ", Path]
-    end,
+    Identity =
+        case proplists:get_value(identity, Options) of
+            undefined ->
+                "";
+            Path ->
+                [" -i ", Path]
+        end,
     % Build SSH command for remote forwarding
-    Cmd = lists:flatten(io_lib:format(
-        "ssh ~s-nNT -R ~b:localhost:~b ~s@~s",
-        [Identity, RemotePort, LocalPort, User, SshHost]
-    )),
+    Cmd = lists:flatten(
+              io_lib:format("ssh ~s-nNT -R ~s:~b:~s:~b ~s@~s",
+                            [Identity, RemoteIp, RemotePort, LocalIp, LocalPort, User, SshHost])),
     start_tunnel(Cmd, remote).
 
 %% Stop a running tunnel
@@ -52,14 +73,14 @@ stop(#tunnel{port = Port} = _Tunnel) ->
 %% Internal functions
 
 start_tunnel(Cmd, Type) ->
+    io:format(">>> Cmd: ~p~n",[Cmd]),
     Port = open_port({spawn, Cmd}, [exit_status, {line, 16384}]),
     monitor_tunnel(Port, Type, []).
 
 monitor_tunnel(Port, Type, Log) ->
     receive
         {Port, {data, {eol, Line}}} ->
-            % Log any output from the SSH process
-            monitor_tunnel(Port, Type, [Line|Log]);
+            monitor_tunnel(Port, Type, [Line | Log]);
         {Port, {exit_status, 0}} ->
             {ok, #tunnel{port = Port, type = Type}};
         {Port, {exit_status, Status}} ->
